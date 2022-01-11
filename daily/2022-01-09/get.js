9c509e7626628400ba3e489e156a3da0edb4d6fc
richLpf <lpengfei66@163.com>
2022-01-11 22:02:32 +0800

    feat: loadash

---
+++ daily/2022-01-09/get.js
@@ -0,0 +1,20 @@
+function get(object, path, defaultValue){
+    const result = object === null ? undefined : baseGet(object, path)
+    return result === undefined ? defaultValue : result
+}
+
+function baseGet(object, path){
+    path = castPath(path, object);
+    return object[path]
+}
+
+function castPath(value, object){
+    if(Array.isArray(value)){
+        return value
+    }
+    return stringToPath(value)
+}
+
+function stringToPath(value){
+    
+}
--- daily/2022-01-09/index.js
+++ daily/2022-01-09/index.js
@@ -1,25 +1,32 @@
+//lodash get
 const object = { a: [{ b: { c: 3 } }] };
 
 function get(object, params, rest){
     if(rest !== undefined) return rest
     let temp = params.split(".")
-    console.log("111111", temp)
     let value = object;
     for(let i = 0; i<temp.length; i++){
-        console.log("temp", value, temp[i])
-        value = value[temp[i]]
+        value = getValue(temp[i], value)
     }
-    console.log("result", value)
     return value
 }
 
-//=> 3
-get(object, "a[0].b.c");
+function getValue(params, result){
+    if(params.indexOf("[")>-1){
+        let objKey = params.split("[")[0]
+        const right = params.split("[")[1]
+        let arrKey = right.substring(0, right.length-1)
+        return result[objKey][arrKey]
+    }
+    return result[params]
+}
 
 //=> 3
-// get(object, 'a[0]["b"]["c"]');
+console.log("1111", get(object, "a[0].b.c"));
+
+//=> 3
+console.log("222", get(object, 'a[0]["b"]["c"]'));
 
 //=> 10086
 let result = get(object, "a[100].b.c", 10086);
 // console.log("result", result)
-
